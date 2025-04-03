1bbd6b7c1fb9d1a16b272845bd249263327b497f
Leon Cvetkovski <cvetkovskileon@gmail.com>
2025-04-03 15:56:01 +0200

    komit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { AuthProvider } from './context/AuthContext'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Hotel Dashboard',
+  title: 'Elarion Insights',
   description: 'Hotel Management Dashboard',
 }
 
